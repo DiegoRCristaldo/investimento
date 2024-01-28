46f08ef60130f4ad0407c009d57976097e51df94
diegodev <diegorcristaldo@hotmail.com>
2024-01-27 23:45:19 -0300

    Renomeando classes

--- assets/Main.java
+++ assets/Main.java
@@ -1,5 +1,7 @@
-public static void Main(string[], args){
-    Ordem ordem = new.Ordem;
-
-    ordem.Compra(15.95, 100);
+public class Main{
+    public static void Main(string[], args){
+        Ordem ordem = new.Ordem;
+    
+        ordem.Compra(15.95, 100);
+    }
 }
